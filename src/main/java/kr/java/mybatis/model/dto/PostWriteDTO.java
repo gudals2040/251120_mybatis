32e4c6eeaa0d4affbc0d434ea28209e843cf7dde
PARK HYEONG MIN <qd0420@gmail.com>
2025-11-20 16:07:44 +0900

    step5 dto

---
+++ src/main/java/kr/java/mybatis/model/dto/PostWriteDTO.java
@@ -0,0 +1,5 @@
+
+package kr.java.mybatis.model.dto;
+
+public record PostWriteDTO(String title, String content, Long memberId) {
+}
---
+++ src/main/java/kr/java/mybatis/service/PostService.java
@@ -0,0 +1,15 @@
+package kr.java.mybatis.service;
+
+import kr.java.mybatis.model.dto.PostDTO;
+import kr.java.mybatis.model.dto.PostUpdateDTO;
+import kr.java.mybatis.model.dto.PostWriteDTO;
+
+import java.util.List;
+
+public interface PostService {
+    void write(PostWriteDTO dto);
+    PostDTO findById();
+    List<PostDTO> findAll();
+    void update(PostUpdateDTO dto);
+    void delete(Long postId);
+}
